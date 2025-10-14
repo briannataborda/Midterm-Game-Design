7f070ea8f717d0c5e1f2038d69877f4a1e27ceee
ebaik01 <ellen.baik@tufts.edu>
2025-10-13 22:26:00 -0400

    restart worksing

---
+++ Midterm_GameDesign/Assets/Scripts/RestartButton.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class RestartButton : MonoBehaviour
+{
+    public void Restart(){
+        if (GameHandler.Instance!=null){
+            GameHandler.Instance.RestartGame();
+        }
+    }
+}
